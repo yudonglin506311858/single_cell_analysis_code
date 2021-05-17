5fb6ae8da93728cf24439b50958ac97cd0aaa9e4
yudonglin506311858 <506311858@qq.com>
2021-05-17 16:13:28 +0800

    Create loom_file.R

---
+++ RNA velocity analysis/loom_file.R
@@ -0,0 +1,11 @@
+conda activate velocity
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/erythropoiesis/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/erythropoiesis_nonpro/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/erythropoiesis_pro/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/nonpro/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/NonPro/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/baso/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/baso2/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/poly/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/ortho/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/reti/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
