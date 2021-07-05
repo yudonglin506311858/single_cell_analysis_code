a1acb6750c7d293ef618aff92ebbc6ec71044b94
yudonglin506311858 <506311858@qq.com>
2021-07-05 20:11:16 +0800

    Update cellranger.sh

--- cellranger.sh
+++ cellranger.sh
@@ -8,3 +8,18 @@ cellranger count --id=neutrophil \
                    --sample=SRR10127227 \
                    --localcores=40 \
                    --localmem=64
+
+#巨噬细胞
+conda activate chipseq
+prefetch -t ascp-a "/data/yudonglin/miniconda3/envs/chipseq/etc/asperaweb_id_dsa.openssh" SRR12493999 -O ~/example
+export PATH=/data/yudonglin/software/cellranger-4.0.0:$PATH
+cellranger count --id=macrophage \
+                   --transcriptome=/data/yudonglin/velocity/refdata-gex-mm10-2020-A \
+                   --fastqs=/data/yudonglin/example/SRR12493999/ \
+                   --sample=SRR12493999 \
+                   --localcores=40 \
+                   --localmem=64
+
+
+
+
