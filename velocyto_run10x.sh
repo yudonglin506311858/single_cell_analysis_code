700aa138fa15527d425de34ebf1046a3b92a8483
yudonglin506311858 <506311858@qq.com>
2021-09-02 10:22:04 +0800

    Update velocyto_run10x.sh

--- velocyto_run10x.sh
+++ velocyto_run10x.sh
@@ -18,3 +18,9 @@ velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/softwa
 
 
 
+export OPENBLAS_NUM_THREADS=1
+conda activate velocity
+velocyto run10x -m /data/yudonglin/velocity/mm10_rmsk.gtf /data/yudonglin/software/cellranger-4.0.0/pro/ /data/yudonglin/velocity/refdata-gex-mm10-2020-A/genes/genes.gtf
+
+
+
